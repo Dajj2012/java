d4335f616a302188469a01bd41aeeba0f784ca38
Alexandr <dajj2012@mail.ru>
2023-04-03 16:11:25 +0300

    3

--- 1/Program.java
+++ 1/Program.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
 public class Program {
+
     public static void main(String[] args) {
-        int a = 3;
-        System.out.println(a);
-        a++;
-        System.out.println(a);
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Введите первое число: ");
+        int i = sc.nextInt();
+        System.out.printf("Треугольное число: %d\n", summ(i));
+        System.out.printf("Факториал: %d\n", fact(i));
+        sc.close();
+    } 
+
+    public static int summ(int a) {
+        return (a * (a + 1)) / 2;
     }
+    public static int fact(int a) {
+        int j=1;
+        for (int i=2; i<a+1; i++){
+            j=j*i;
+        }
+        return j;
+    }
+
+
 }
